a288d48c879d1bd5f86882f0d10c424f20c15647
Victor Sosa <victorjsbl@gmail.com>
2015-11-15 19:35:59 -0600

    v4
    Solo falta el problema con el delete y que se vea

--- platforms/android/assets/www/ver.js
+++ platforms/android/assets/www/ver.js
@@ -16,25 +16,30 @@ function getDirSuccess(dirEntry) {
     directoryReader.readEntries(readerSuccess, fail);
 }
 
+var divsTotales;
 function readerSuccess(entries) {
     var i;
+    divsTotales = entries.length;
     var lista = document.getElementById("archivos");
 
     for (i=0; i<entries.length; i++) {
         lista.innerHTML +=
         "<div class=\"lista\" id=\""+i+"\" onclick=clickMostrar("+i+");>" + 
-            "<img src=\"img/repoop.png\"> " + entries[i].name + 
+            "<img src=\"img/repoop.png\">" + entries[i].name.slice(0, -4) + // el slice le quita la terminación
         "</div><br>";
     }
 }
-
 var toques = 0;
 var ultimaFila;
+var divSeleccionado;
 // valida que se haga doble tap en un div antes de mostrar el contenido del archivo
 function clickMostrar(fila){
     if(ultimaFila == null){
         ultimaFila = fila;
     }
+
+    colorearSeleccion(fila);
+    divSeleccionado = fila;
 
     if(ultimaFila == fila){
         toques++;
@@ -46,6 +51,32 @@ function clickMostrar(fila){
         toques = 1;
     }
     ultimaFila = fila;
+    divSeleccionado = ultimaFila;
+}
+
+function colorearSeleccion(seleccionado){
+    for (var i = 0; i < divsTotales; i++) {
+        document.getElementById(i).style.color = "black";
+    }
+    document.getElementById(seleccionado).style.color = "LightBlue";
+}
+
+function borraArchivo(){
+    var titulo = document.getElementById(ultimaFila).innerText;
+    if(titulo != "undefined"){
+        deleteFile(titulo + ".txt");
+    } else {
+        alert("No se está seleccionando un archivo para borrar.");
+    }
+}
+
+function deleteFile(filename){
+    var direccionArchivo = "proyecto/" + filename;
+    alert("Se va a intentar borrar el archivo: " + direccionArchivo); 
+    fileSystem.root.getFile(direccionArchivo, {create: true}, function(fileEntry) {
+        fileEntry.remove();
+    });
+    alert("Se debió haber borrado el " + filename);
 }
 
 function mostrarArchivo(){
